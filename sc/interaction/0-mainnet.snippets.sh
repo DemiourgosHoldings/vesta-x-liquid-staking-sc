5ce11ab341f6b6e5981cb066c03d9876b66e06a8
icegriffinguru <icegriffin1122@gmail.com>
2023-03-10 02:33:00 -0500

    [Ice] mainnet update with audit fix

---
+++ sc/interaction/0-mainnet.snippets.sh
@@ -0,0 +1,25 @@
+PROXY=https://gateway.multiversx.com
+CHAIN_ID="1"
+WALLET="./wallets/vestax-vegld.pem"
+ADDRESS=$(mxpy data load --key=address-mainnet)
+######################################################################
+
+DEPLOY_GAS_LIMIT=120000000
+
+deploy() {
+    mxpy --verbose contract deploy  --project=${PROJECT} --recall-nonce --pem=${WALLET} --send --proxy=${PROXY} --chain=${CHAIN_ID} \
+    --outfile="deploy-mainnet.interaction.json" \
+    --gas-limit=${DEPLOY_GAS_LIMIT}
+    
+    ADDRESS=$(mxpy data parse --file="deploy-mainnet.interaction.json" --expression="data['contractAddress']")
+
+    mxpy data store --key=address-mainnet --value=${ADDRESS}
+
+    echo ""
+    echo "Smart contract address: ${ADDRESS}"
+}
+
+upgrade() {
+    mxpy --verbose contract upgrade ${ADDRESS} --project=${PROJECT} --recall-nonce --pem=${WALLET} --send --outfile="upgrade.json" --proxy=${PROXY} --chain=${CHAIN_ID} \
+    --gas-limit=${DEPLOY_GAS_LIMIT}
+}
